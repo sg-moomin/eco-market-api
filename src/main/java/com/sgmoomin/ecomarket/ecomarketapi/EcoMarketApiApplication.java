4b569fccfdcda740e13f2b97b14b9a0c9750c674
sg-moomin <rooney9325@naver.com>
2023-03-05 10:24:08 +0900

    first commit

---
+++ src/main/java/com/sgmoomin/ecomarket/ecomarketapi/EcoMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.sgmoomin.ecomarket.ecomarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketApiApplication.class, args);
+	}
+
+}
